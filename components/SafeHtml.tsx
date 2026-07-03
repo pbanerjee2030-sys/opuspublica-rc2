f0009816d16b421ad5fa6c0eeec02731b1d83455
pbanerjee2030-sys <p.banerjee2030@gmail.com>
2026-07-03 06:00:07 +0200

    Mission 14: fix XSS regression in SafeHtml sanitization

--- components/SafeHtml.tsx
+++ components/SafeHtml.tsx
@@ -1,7 +1,6 @@
 'use client';
 
-import { useState, useEffect } from 'react';
-import DOMPurify from 'dompurify';
+import DOMPurify from 'isomorphic-dompurify';
 
 interface Props {
   html: string;
@@ -9,15 +8,10 @@ interface Props {
 }
 
 export default function SafeHtml({ html, className }: Props) {
-  const [clean, setClean] = useState(html);
-
-  useEffect(() => {
-    const sanitized = DOMPurify.sanitize(html, {
-      ALLOWED_TAGS: ['p', 'br', 'strong', 'em', 'u', 'ol', 'ul', 'li', 'a', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'blockquote', 'code', 'pre', 'sup', 'sub', 'span', 'div'],
-      ALLOWED_ATTR: ['href', 'target', 'rel', 'class', 'style'],
-    });
-    setClean(sanitized);
-  }, [html]);
+  const clean = DOMPurify.sanitize(html, {
+    ALLOWED_TAGS: ['p', 'br', 'strong', 'em', 'u', 'ol', 'ul', 'li', 'a', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'blockquote', 'code', 'pre', 'sup', 'sub', 'span', 'div'],
+    ALLOWED_ATTR: ['href', 'target', 'rel', 'class', 'style'],
+  });
 
   return <div className={className} dangerouslySetInnerHTML={{ __html: clean }} />;
 }
